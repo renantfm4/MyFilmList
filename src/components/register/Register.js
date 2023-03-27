41d6734f494af66b6c0865cb28d601cc560763b0
Renan Araújo <111506459+renantfm4@users.noreply.github.com>
2023-03-27 13:18:38 -0300

    Create Register.js

---
+++ src/components/register/Register.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import './Register.css';
+
+const Register = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    function registrar(r){
+        r.preventDefault();
+        const datas = [{
+            'email': email,
+            'password': password,
+        }]
+
+        localStorage.setItem('Dados', JSON.stringify(datas));
+
+        r.target.reset();
+    }
+
+    const goToRegisterPage = () => {
+        navigate('/')
+      }
+  
+    return(
+        <main className='centralize'>
+            <form onSubmit={registrar}>
+                <div className="style2">
+                    <h1>Cadastrar</h1>
+                </div>
+                <input type="email" onChange={e=> setEmail(e.target.value)}></input>
+                <input type="password" onChange={e=> setPassword(e.target.value)}></input>
+                <button type="submit" className="salvar">Salvar</button>
+                <div className="cadastrado">
+                <button type="button" className='cadastrado' onClick={goToRegisterPage}>Já Cadastrado</button>
+                </div>
+             </form>
+        </main>
+    )
+  }
+
+export default Register;
