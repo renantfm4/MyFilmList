92fb807be09cf01a5d787e7a77166226dce58aa0
Renan Araújo <111506459+renantfm4@users.noreply.github.com>
2023-03-27 13:12:49 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Login from './components/login/Login'
+import Register from './components/register/Register'
+import Home from './components/home/index'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { AuthProvider } from './components/Auth';
+
+export default function App(){
+
+  return(
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
+      </AuthProvider>
+    </Router>
+    
+  )
+}
